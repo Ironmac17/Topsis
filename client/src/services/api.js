73baac3b8175f3585ff9a87cb98e5ee6adccf934
Ironmac17 <nimish4agrawal@gmail.com>
2026-01-20 13:42:07 +0530

    Api frontned fix

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://topsis-backend.vercel.app/api",
+  baseURL: "https://topsis-3s7c.vercel.app/api",
 });
 
 export default api;
