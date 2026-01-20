fbb457a1721b147509d96b7638d4ff5fe40fdb40
Ironmac17 <nimish4agrawal@gmail.com>
2026-01-20 13:21:45 +0530

    Api change

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://topsis.up.railway.app/api",
+  baseURL: "https://topsis-backend.vercel.app/api",
 });
 
 export default api;
